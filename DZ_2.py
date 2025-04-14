92083588baa69da65af38023b1823f09669545ec
Vlad <vladkuznecov271@gmail.com>
2025-04-14 17:24:36 +0300

    Create DZ_2.py

---
+++ DZ_2.py
@@ -0,0 +1,21 @@
+import re
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+
+
+def generator_numbers(text: str):
+    pattern = r'[0-9]+\.\d+' # Числа та числа із плаваючою точкою
+    matching = re.findall(pattern, text)
+
+    for number in matching:
+        yield float(number)
+
+
+def sum_profit(text: str, func):
+    return sum(func(text))
+
+
+print(sum_profit(text, generator_numbers))
+
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
